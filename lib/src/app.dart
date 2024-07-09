3ffbe82ef6847c97b497d4607a86a2f2e28c8c3a
Jose <jfrometa@gmal.com>
2024-07-08 22:12:01 -0400

    updates for android and web, new onboarding, vertex AI workin

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:starter_architecture_flutter_firebase/src/routing/app_router.dart';
+import 'package:starter_architecture_flutter_firebase/src/theme/app_theme.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
@@ -12,33 +13,35 @@ class MyApp extends ConsumerWidget {
     final goRouter = ref.watch(goRouterProvider);
     return MaterialApp.router(
       routerConfig: goRouter,
-      theme: ThemeData(
-        colorSchemeSeed: primaryColor,
-        unselectedWidgetColor: Colors.grey,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: primaryColor,
-          foregroundColor: Colors.white,
-          elevation: 2.0,
-          centerTitle: true,
-        ),
-        scaffoldBackgroundColor: Colors.grey[200],
-        dividerColor: Colors.grey[400],
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: primaryColor,
-            foregroundColor: Colors.white,
-          ),
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            backgroundColor: primaryColor,
-            foregroundColor: Colors.white,
-          ),
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: primaryColor,
-        ),
-      ),
+      theme: AppTheme.getLight(),
+
+      //  ThemeData(
+      //   colorSchemeSeed: primaryColor,
+      //   unselectedWidgetColor: Colors.grey,
+      //   appBarTheme: const AppBarTheme(
+      //     backgroundColor: primaryColor,
+      //     foregroundColor: Colors.white,
+      //     elevation: 2.0,
+      //     centerTitle: true,
+      //   ),
+      //   scaffoldBackgroundColor: Colors.grey[200],
+      //   dividerColor: Colors.grey[400],
+      //   elevatedButtonTheme: ElevatedButtonThemeData(
+      //     style: ElevatedButton.styleFrom(
+      //       backgroundColor: primaryColor,
+      //       foregroundColor: Colors.white,
+      //     ),
+      //   ),
+      //   outlinedButtonTheme: OutlinedButtonThemeData(
+      //     style: OutlinedButton.styleFrom(
+      //       backgroundColor: primaryColor,
+      //       foregroundColor: Colors.white,
+      //     ),
+      //   ),
+      //   floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      //     backgroundColor: primaryColor,
+      //   ),
+      // ),
       debugShowCheckedModeBanner: false,
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,16 +7,20 @@ import Foundation
 
 import cloud_firestore
 import desktop_webview_auth
+import firebase_app_check
 import firebase_auth
 import firebase_core
+import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
