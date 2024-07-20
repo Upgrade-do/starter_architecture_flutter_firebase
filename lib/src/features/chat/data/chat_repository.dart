df2fe1c2900bdc4423cbf5a6eb7ddfb3188bcd00
Jose <jfrometa@gmal.com>
2024-07-20 13:31:44 -0400

    prompt work in progress

--- lib/src/features/chat/data/chat_repository.dart
+++ lib/src/features/chat/data/chat_repository.dart
@@ -6,6 +6,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:starter_architecture_flutter_firebase/src/extensions/extensions.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/chat/data/storage_repository.dart';
 import 'package:starter_architecture_flutter_firebase/src/features/chat/domain/message.dart';
+import 'package:starter_architecture_flutter_firebase/src/features/prompt/prompt_model.dart';
 import 'package:uuid/uuid.dart';
 
 //   final model =
@@ -180,5 +181,60 @@ class ChatRepository {
     } catch (e) {
       throw Exception(e.toString());
     }
+  }
+}
+
+class GeminiService {
+  static Future<GenerateContentResponse> generateContent(
+      GenerativeModel model, PromptData prompt) async {
+    if (prompt.images.isEmpty) {
+      return await GeminiService.generateContentFromText(model, prompt);
+    } else {
+      return await GeminiService.generateContentFromMultiModal(model, prompt);
+    }
+  }
+
+  static Future<GenerateContentResponse> generateContentFromMultiModal(
+      GenerativeModel model, PromptData prompt) async {
+    final mainText = TextPart(prompt.textInput);
+    final additionalTextParts =
+        prompt.additionalTextInputs.map((t) => TextPart(t));
+    final imagesParts = <DataPart>[];
+
+    for (var f in prompt.images) {
+      final bytes = await (f.readAsBytes());
+      imagesParts.add(DataPart('image/jpeg', bytes));
+    }
+
+    final input = [
+      Content.multi([...imagesParts, mainText, ...additionalTextParts])
+    ];
+
+    return await model.generateContent(
+      input,
+      generationConfig: GenerationConfig(
+        temperature: 0.4,
+        topK: 32,
+        topP: 1,
+        maxOutputTokens: 4096,
+      ),
+      safetySettings: [
+        SafetySetting(HarmCategory.harassment, HarmBlockThreshold.high),
+        SafetySetting(HarmCategory.hateSpeech, HarmBlockThreshold.high),
+      ],
+    );
+  }
+
+  static Future<GenerateContentResponse> generateContentFromText(
+      GenerativeModel model, PromptData prompt) async {
+    final mainText = TextPart(prompt.textInput);
+    final additionalTextParts =
+        prompt.additionalTextInputs.map((t) => TextPart(t)).join("\n");
+
+    return await model.generateContent([
+      Content.text(
+        '${mainText.text} \n $additionalTextParts',
+      )
+    ]);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import cloud_firestore
 import desktop_webview_auth
+import device_info_plus
 import file_selector_macos
 import firebase_app_check
 import firebase_auth
@@ -20,6 +21,7 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
