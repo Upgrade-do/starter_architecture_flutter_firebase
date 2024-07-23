8e2c2b2a64e75115a317a14610d88306146f55d1
jose frometa <josefrometa@holistika.co>
2024-07-23 19:35:04 -0400

    template

--- lib/src/features/dialog_flow_cx/data/dialog_flow_client.dart
+++ lib/src/features/dialog_flow_cx/data/dialog_flow_client.dart
@@ -6,44 +6,22 @@ import 'package:googleapis/dialogflow/v3.dart' as df;
 import 'package:http/http.dart' as http;
 
 class AuthClient {
-  static const _scopes = [df.DialogflowApi.dialogflowScope];
-  static var _dialogFlowApi ;
+  static const _scopes = [df.DialogflowApi.cloudPlatformScope];
 
   static Future<df.DialogflowApi> getDialogflowApi() async {
  
         // Load the service account JSON file from assets
         final serviceAccountJson = await rootBundle.loadString('assets/key.json');
         final credentials = ServiceAccountCredentials.fromJson(json.decode(serviceAccountJson));
-        debugPrint(' SERVICE JSON: \n $serviceAccountJson');
-        
+            debugPrint(' SERVICE JSON: \n $serviceAccountJson');
         final client = await clientViaServiceAccount(credentials, _scopes);
- 
     
         // Specify the base URL for the correct region
         const endpoint = 'https://us-dialogflow.googleapis.com/'; // {us-central1}
-        _dialogFlowApi = df.DialogflowApi(client, rootUrl: endpoint);
-        return _dialogFlowApi;
+        return df.DialogflowApi(client, rootUrl: endpoint);
+ 
+ 
   }
-
-  // Use service account credentials to obtain oauth credentials.
-static Future<AccessCredentials?> obtainCredentials() async {
-        // Load the service account JSON file from assets
-        try {
-        final serviceAccountJson =  ServiceAccountCredentials.fromJson('assets/key.json');
-        // final credentials = ServiceAccountCredentials.fromJson(json.decode(serviceAccountJson));
-
-        var client = http.Client();
-        AccessCredentials credentials =
-          await obtainAccessCredentialsViaServiceAccount(serviceAccountJson, _scopes, client);
-
-        client.close();
-        return credentials;
-
-        } catch (e) {
-          debugPrint(e.toString());
-        }
-
-}
 }
 
 class ChatbotClient {
@@ -54,38 +32,22 @@ class ChatbotClient {
   ChatbotClient({required this.projectId, required this.agentId, required this.location});
 
   Future<String> sendMessage(String sessionId, String message) async {
-    final dialogflow = await AuthClient.obtainCredentials();
+    final dialogflow = await AuthClient.getDialogflowApi();
     final sessionPath = 'projects/$projectId/locations/$location/agents/$agentId/sessions/$sessionId';
     final queryInput = df.GoogleCloudDialogflowCxV3QueryInput(languageCode: 'en',
       text: df.GoogleCloudDialogflowCxV3TextInput(text: message),
     );
 
-     print(dialogflow.toString());
-    // final response = await dialogflow.projects.locations.agents.sessions.detectIntent(
-    //   df.GoogleCloudDialogflowCxV3DetectIntentRequest(queryInput: queryInput),
-    //   sessionPath,
-    // );
-
-    // final queryResult = response.queryResult;
-    // if (queryResult != null && queryResult.responseMessages != null && queryResult.responseMessages!.isNotEmpty) {
-    //   return queryResult.responseMessages!.first.text!.text!.first;
-    // } else {
-    //   return 'No response from chatbot';
-    // }
-
-    return 'dialog flow ${dialogflow}';
-  }
-
-    Future<String> detectIntent(String projectId, String sessionId, String text) async {
-    final sessionPath = 'projects/$projectId/locations/global/agent/sessions/$sessionId';
-    final request = df.GoogleCloudDialogflowCxV3DetectIntentRequest(
-      queryInput: df.GoogleCloudDialogflowCxV3QueryInput(
-        text: df.GoogleCloudDialogflowCxV3TextInput(text: text),
-      ),
+    final response = await dialogflow.projects.locations.agents.sessions.detectIntent(
+      df.GoogleCloudDialogflowCxV3DetectIntentRequest(queryInput: queryInput),
+      sessionPath,
     );
 
-    final response = await AuthClient._dialogFlowApi.projects.locations.agent.sessions
-        .detectIntent(request, sessionPath);
-    return response.queryResult?.fulfillmentText ?? 'No response';
+    final queryResult = response.queryResult;
+    if (queryResult != null && queryResult.responseMessages != null && queryResult.responseMessages!.isNotEmpty) {
+      return queryResult.responseMessages!.first.text!.text!.first;
+    } else {
+      return 'No response from chatbot';
+    }
   }
 }
